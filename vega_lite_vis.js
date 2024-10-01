7958904a2361eada0b7a2b0ba3ca1a1dc6d5fb0c
juneveryday <jjin0031@student.monash.edu>
2024-10-01 23:57:48 +0800

    20141001 Water

--- vega_lite_vis.js
+++ vega_lite_vis.js
@@ -1,4 +1,4 @@
 var vg_1 = "1_Household_income.json"; 
-vegaEmbed("#map_chart", vg_1).then(function(result) {
+vegaEmbed("#map_projection", vg_1).then(function(result) {
   // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
