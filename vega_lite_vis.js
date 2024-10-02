e2c520ec2e2b067c57bdf8e86a3d422e081ce6d1
juneveryday <jjin0031@student.monash.edu>
2024-10-03 00:21:46 +0800

    20241003 Graph with brushing done

--- vega_lite_vis.js
+++ vega_lite_vis.js
@@ -5,3 +5,7 @@ vegaEmbed("#map_chart_1", vg_domestic).then(function(result) {
 var vg_nondomestic = "2. Water consumption nondomestic.json"; 
 vegaEmbed("#map_chart_2", vg_nondomestic).then(function(result) {
 }).catch(console.error);
+
+var vg_production = "3. water_production_graph_brushing.json"; 
+vegaEmbed("#map_chart_3", vg_production).then(function(result) {
+}).catch(console.error);
